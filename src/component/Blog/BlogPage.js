b8d8647539923106202512e8c9bc486261adf8cc
Dima Kelasev <dimakelaseff@gmail.com>
2021-09-12 18:32:38 +0300

    add new format for article

--- src/component/Blog/BlogPage.js
+++ src/component/Blog/BlogPage.js
@@ -1,133 +1,144 @@
-import * as React from 'react';
-import Prismic from '@prismicio/client';
-import {RichText, Date} from 'prismic-reactjs';
-import {Link} from "react-router-dom";
-import './style.css';
-import Loader from './components/Loader';
+import * as React from "react";
+import Prismic from "@prismicio/client";
+import { RichText, Date } from "prismic-reactjs";
+import { Link } from "react-router-dom";
+import "./style.css";
+import Loader from "./components/Loader";
 
-import {apiEndpoint, accessToken} from '../../data/constants';
-import {useDispatch, useSelector} from "react-redux";
+import { apiEndpoint, accessToken } from "../../data/constants";
+import { useDispatch, useSelector } from "react-redux";
 
-import {getRandomArray} from "../../service/service.js";
+import { getRandomArray } from "../../service/service.js";
 
 const BlogPage = () => {
-    const [posts, setPosts] = React.useState([]);
-    const [loading, setLoading] = React.useState(false);
-    const [selectedTag, setSelectedTag] = React.useState('');
-    const [arrayTag, setArrayTag] = React.useState([])
-    const [active, setIsActive] = React.useState(false)
-    const dispatch = useDispatch();
-    // const posts = useSelector((state) => state.postReducers.posts);
+  const [posts, setPosts] = React.useState([]);
+  const [postsWIthPhoto, setPostsWIthPhoto] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [selectedTag, setSelectedTag] = React.useState("");
+  const [arrayTag, setArrayTag] = React.useState([]);
+  const [active, setIsActive] = React.useState(false);
+  const dispatch = useDispatch();
+  // const posts = useSelector((state) => state.postReducers.posts);
 
+  const client = Prismic.client(apiEndpoint, { accessToken });
 
-    const client = Prismic.client(apiEndpoint, {accessToken});
-
-    const fetchData = async () => {
-        setLoading(true);
-        try {
-            const params = [Prismic.Predicates.at('document.type', 'blog')];
-            if (selectedTag) {
-                params.push(Prismic.Predicates.at('document.tags', [selectedTag]));
-            }
-            const response = await client.query(params);
-            if (response) {
-                setPosts(response.results)
-                if (!arrayTag.length) {
-                    const tags = response.results.map(obj => obj.tags).flat();
-                    const newEl = getRandomArray(tags)
-                    setArrayTag(newEl)
-                }
-                // dispatch({type: 'UPDATE_POSTS', dataPosts: response.results})
-            }
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const params = [Prismic.Predicates.at("document.type", "blog")];
+      const secondParams = [
+        Prismic.Predicates.at("document.type", "blog_with_photo"),
+      ];
+      if (selectedTag) {
+        params.push(Prismic.Predicates.at("document.tags", [selectedTag]));
+      }
+      const response = await client.query(params);
+      const responseWithPhoto = await client.query(secondParams);
+      if (response) {
+        setPosts(response.results);
+        if (!arrayTag.length) {
+          const tags = response.results.map((obj) => obj.tags).flat();
+          const newEl = getRandomArray(tags);
+          setArrayTag(newEl);
         }
-    }
-
-    React.useEffect(() => {
-
-        fetchData();
-    }, [])
-
-    React.useEffect(() => {
-        fetchData()
-    }, [selectedTag])
-
-    const formateDate = (date) => {
-        const data = Date(date)
-        const formattedDate = Intl.DateTimeFormat('en-US', {
-            formatMatche: "basic",
-            year: "numeric",
-            month: "long",
-            day: "numeric"
-        }).format(data);
-
-        return formattedDate
-    }
-
-    const sortArray = (array) => {
-        array.sort(function (a, b) {
-            const dateA = new Date(a.data.date);
-            const dateB = new Date(b.data.date);
-
-            return dateB - dateA
-        })
-    }
-    sortArray(posts)
-
-    const handleChangeBackground = (tag) => {
-        setIsActive(!active)
-        setSelectedTag(tag)
-        if (selectedTag) {
-            setSelectedTag('')
+        if (responseWithPhoto) {
+          setPostsWIthPhoto(responseWithPhoto.results);
         }
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    if (loading) {
-        return <Loader/>
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  React.useEffect(() => {
+    fetchData();
+  }, [selectedTag]);
+
+  const formateDate = (date) => {
+    const data = Date(date);
+    const formattedDate = Intl.DateTimeFormat("en-US", {
+      formatMatche: "basic",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }).format(data);
+
+    return formattedDate;
+  };
+
+  const sortArray = (array) => {
+    array.sort(function (a, b) {
+      const dateA = new Date(a.data.date);
+      const dateB = new Date(b.data.date);
+
+      return dateB - dateA;
+    });
+  };
+  sortArray(posts);
+
+  const handleChangeBackground = (tag) => {
+    setIsActive(!active);
+    setSelectedTag(tag);
+    if (selectedTag) {
+      setSelectedTag("");
     }
+  };
 
+  if (loading) {
+    return <Loader />;
+  }
+  console.log(posts);
 
-    return (
-        <div className="blogWraper">
-            {/*<input*/}
-            {/*type='text'*/}
-            {/*placeholder='search'*/}
-            {/*value={searchTerm}*/}
-            {/*onChange={handleChange}*/}
-            {/*/>*/}
-            <div className='search_tags '>
-                {arrayTag.slice(0, 5).map((tag, idx) => (
-                    <p key={idx} className={selectedTag === tag ? 'tags activeTags' : 'tags'}
-                       onClick={() => handleChangeBackground(tag)}>{`#${tag}`}</p>
-                ))}
+  return (
+    <div className="blogWraper">
+      {/*<input*/}
+      {/*type='text'*/}
+      {/*placeholder='search'*/}
+      {/*value={searchTerm}*/}
+      {/*onChange={handleChange}*/}
+      {/*/>*/}
+      <div className="search_tags ">
+        {arrayTag.slice(0, 5).map((tag, idx) => (
+          <p
+            key={idx}
+            className={selectedTag === tag ? "tags activeTags" : "tags"}
+            onClick={() => handleChangeBackground(tag)}
+          >{`#${tag}`}</p>
+        ))}
+      </div>
+
+      {posts.map((post, idx) => (
+        <div key={idx}>
+          <div className="img-back"></div>
+          <h1 className="blog_title">{RichText.asText(post.data.title)}</h1>
+          <div className="blog_wraper_subtitle">
+            <p className="blog_subtitle">{RichText.asText(post.data.type)}</p>
+            <p className="blog_dot">&bull;</p>
+            <p className="blog_data">{formateDate(post.data.date)}</p>
+          </div>
+          <div className="blog_withLink">
+            <div className="blog_tags">
+              {post.tags.map((tag) => (
+                <p>{`#${tag}`}</p>
+              ))}
             </div>
-
-
-            {posts.map((post, idx) => (
-                <div key={idx}>
-                    <div className="img-back"></div>
-                    <h1 className='blog_title'>{RichText.asText(post.data.title)}</h1>
-                    <div className='blog_wraper_subtitle'>
-                        <p className='blog_subtitle'>{RichText.asText(post.data.type)}</p>
-                        <p className='blog_dot'>&bull;</p>
-                        <p className='blog_data'>{formateDate((post.data.date))}</p>
-                    </div>
-                    <div className='blog_withLink'>
-                        <div className='blog_tags'>{post.tags.map(tag =>
-                            <p>{`#${tag}`}</p>)}</div>
-                        <div className='blog_preview'>
-                            <div className='blog_text'>{RichText.asText(post.data.text)}</div>
-                        </div>
-                        <Link className='link_BlogPost' to={`${post.id}`}>Смотреть больше...</Link>
-
-                    </div>
-                </div>
-            ))}
+            <div className="blog_preview">
+              <div className="blog_text">{RichText.asText(post.data.text)}</div>
+            </div>
+            <Link className="link_BlogPost" to={`${post.id}`}>
+              Смотреть больше...
+            </Link>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default React.memo(BlogPage);
--- src/component/Blog/components/Post.js
+++ src/component/Blog/components/Post.js
@@ -1,20 +1,19 @@
-import * as React from 'react';
-import {apiEndpoint, accessToken} from '../../../data/constants';
-import {RichText} from 'prismic-reactjs';
-import Prismic from '@prismicio/client';
+import * as React from "react";
+import { apiEndpoint, accessToken } from "../../../data/constants";
+import { RichText } from "prismic-reactjs";
+import Prismic from "@prismicio/client";
 import Loader from "./Loader";
-import {useParams} from "react-router-dom";
-import {Link} from "react-router-dom";
-import NotFound from '../../NotFound/NotFound'
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import NotFound from "../../NotFound/NotFound";
 
 const Post = () => {
   const [post, setPost] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
-  const {id} = useParams()
-
+  const { id } = useParams();
 
   React.useEffect(() => {
-    const client = Prismic.client(apiEndpoint, {accessToken});
+    const client = Prismic.client(apiEndpoint, { accessToken });
     setLoading(true);
     const fetchData = async () => {
       try {
@@ -25,34 +24,52 @@ const Post = () => {
       } finally {
         setLoading(false);
       }
-    }
+    };
     if (id) {
       fetchData();
     }
-  }, [])
+  }, []);
 
-
+  console.log(post);
 
   if (loading) {
-    return <Loader/>
+    return <Loader />;
   }
 
   if (!post) {
-    return <NotFound/>;
+    return <NotFound />;
   }
 
   return (
     <div className="blogWraper">
-      <Link className='linkBack' to='/Blog'>Back</Link>
-      <h1 className='blog_title blog_title_Post'>{RichText.asText(post.data.title)}</h1>
-      <img className='imgBlog' src={post.data.logo_page.url}/>
-      <div className='blog_text'>{RichText.render(post.data.text)}</div>
-      <Link className='linkBackMobile' to='/Blog'>Back</Link>
+      <Link className="linkBack" to="/Blog">
+        Back
+      </Link>
+      <h1 className="blog_title blog_title_Post">
+        {RichText.asText(post.data.title)}
+      </h1>
+      <div className="blog_text">
+        {RichText.render(post.data.first_article)}
+      </div>
+      <img className="imgBlog" src={post.data.logo_page.url} />
+      <div className="blog_text">{RichText.render(post.data.text)}</div>
+      <img className="imgBlog" src={post.data.img_1.url} />
+      <img className="imgBlog" src={post.data.img_2.url} />
+      <img className="imgBlog" src={post.data.img_3.url} />
+      <div className="blog_text">{RichText.render(post.data.text_2)}</div>
+      <img className="imgBlog" src={post.data.img_4.url} />
+      <img className="imgBlog" src={post.data.img_41.url} />
+      <div className="blog_text">{RichText.render(post.data.text_3)}</div>
+      <img className="imgBlog" src={post.data.img_5.url} />
+      <div className="blog_text">{RichText.render(post.data.text_4)}</div>
+      <img className="imgBlog" src={post.data.img_6.url} />
+      <div className="blog_text">{RichText.render(post.data.end)}</div>
+      <img className="imgBlog" src={post.data.last_foto.url} />
+      <Link className="linkBackMobile" to="/Blog">
+        Back
+      </Link>
     </div>
-
   );
 };
 
 export default Post;
-
-
