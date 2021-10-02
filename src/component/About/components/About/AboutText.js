66de1eff1ec3870b20a358eca70ac1530ff18d9a
Dima Kelasev <dimakelaseff@gmail.com>
2021-10-02 16:15:34 +0300

    fix sction about

--- src/component/About/components/About/AboutText.js
+++ src/component/About/components/About/AboutText.js
@@ -11,20 +11,17 @@ const AboutText = () => {
         <img className="avatar" src={avatar} alt="" />
         <h1>Dmitriy Kelasev </h1>
         <p className="info__subtitle">Web developer</p>
-        <h3>Here I will tell you about myself!</h3>
 
         <p className="content__introduce">
-          As you already understood, my name is Dima, I am 29 years old at the
-          time of writing this blog. More than five years my main job was
-          coaching, I have coached people who are ready to compete. But this
-          came to an end, more than 1.5 years. I've been programming. So also i
-          doing active terekin, climbing. My friend and I are taking people to
-          the mountains, "join us." And yet this blog will be more about my
-          history and my journey into programming.
+          Hi, my name is Dmitriy and this my blog.
+          <br />
+          I'm a web developer with over two years of professional background in
+          the field of front-end development. The primary technology stack is
+          React.js/Redux. I prefer React.js as the most modern and useful
+          technology that is suitable for almost every project. It provides high
+          quality along with a quick development process.
         </p>
-        <p className="content__introduce">
-          Here is my technology stack that I managed to master:
-        </p>
+        <p className="content__introduce">Here is my technology skills:</p>
         <TextSkil />
       </div>
     </main>
